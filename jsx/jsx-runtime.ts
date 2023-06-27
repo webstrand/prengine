377fbb8b6571b10e9f1b3169ce291e1e16123656
webstrand <webstrand@gmail.com>
2023-06-27 00:38:28 -0400

    Improve jsx-runtime JSX namespace typings

--- jsx/jsx-runtime.ts
+++ jsx/jsx-runtime.ts
@@ -3,13 +3,38 @@ export {jsx, jsx as jsxs, jsx as Fragment}
 type DOMElement = Element;
 declare global {
 	namespace JSX {
+		// Result type of all JSX expressions
 		type Element = DOMElement;
+
+		// Type that all function components must be compatible with.
+		// The `| string` is needed for intrinsics, I choose not to double
+		// the check against IntrinsicElements.
+		type ElementType = (() => Element) | string;
+
+		// Type that all class component instances must satisfy. By setting to
+		// never we disable class components. Also disabled by ElementType.
+		type ElementClass = never;
+
+		// Deprecated. Specifies the class instance property used for
+		// type-checking the types of attributes on custom component classes.
+		interface ElementAttributesProperty {
+			// Empty implies that we use all properties on the class instance
+			// as arguments.
+		}
+
+		// Specifies which attribute name to use for type-checking jsx element
+		// children.
+		interface ElementChildrenAttribute {
+			children: {};
+		}
+
 		type ChildElement = string | number | boolean | symbol | bigint | null | undefined | { toString(): string } | (() => ChildElements) | Node;
 		type ChildElements = ChildElement | Iterable<ChildElement>;
 
 		interface HTMLAttributes<T> {
 			id?: string;
 			class?: string;
+			children?: ChildElements;
 		}
 
 		interface HTMLImageAttributes<T extends HTMLImageElement> extends HTMLAttributes<T> {
@@ -31,6 +56,17 @@ declare global {
 			a: HTMLAnchorAttributes<HTMLAnchorElement>;
 			script: HTMLScriptAttributes<HTMLScriptElement>;
 		}
+
+		interface IntrinsicAttributes {
+		}
+
+		// Special attributes only for class components
+		interface IntrinsicClassAttributes<T> {
+		}
+
+		// Transforms the types of class component attributes based on the
+		// underlying instance type.
+		type LibraryManagedAttributes<T, Props> = Props;
 	}
 }
 
@@ -65,7 +101,7 @@ function expandChildren(children: JSX.ChildElements): (string | Node)[] {
 }
 
 
-function jsx<T extends keyof JSX.IntrinsicElements, Attrs extends JSX.IntrinsicElements[T] & { children?: JSX.ChildElements }>(tag: string | ((attrs: Attrs) => Element), attrs: Attrs) {
+function jsx<T extends keyof JSX.IntrinsicElements, Attrs extends JSX.IntrinsicElements[T]>(tag: string | ((attrs: Attrs) => JSX.ElementType), attrs: Attrs) {
 	if(typeof tag === "function") {
 		if(tag === jsx as never) {
 			const fragment = document.createDocumentFragment();
